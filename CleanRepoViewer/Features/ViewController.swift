f44501ae40f4b4f4acdcf796e60ec69fabe353c0
Miksa <malaxa2000@gmail.com>
2025-12-22 12:22:07 +0300

    chores: base app's structure setup

---
+++ CleanRepoViewer/Features/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CleanRepoViewer
+//
+//  Created by Miksa on 22.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupUI()
+    }
+    
+    private func setupUI() {
+        let label = UILabel()
+        label.text = "Clean Repo Viewer"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
