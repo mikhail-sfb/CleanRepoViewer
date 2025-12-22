5ab2cb8360e452e41825dd9fb5260e8fb234cb90
Miksa <malaxa2000@gmail.com>
2025-12-22 11:51:34 +0300

    Initial Commit

---
+++ CleanRepoViewerTests/CleanRepoViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanRepoViewerTests.swift
+//  CleanRepoViewerTests
+//
+//  Created by Miksa on 22.12.25.
+//
+
+import Testing
+@testable import CleanRepoViewer
+
+struct CleanRepoViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
